0e4ba86224f3d75ca73760cbcb19e25d9c6f0ef4
Osama Abdul Sabboor <56756079+osamaabdulsabboor@users.noreply.github.com>
2020-12-29 21:07:39 +0500

    update

--- client/src/components/about page components/Team.jsx
+++ client/src/components/about page components/Team.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon } from "mdbreact";
+import img from "./Images/zeeshan.jpeg";
 
 const TeamPage = () => {
   return (
@@ -18,7 +19,7 @@ const TeamPage = () => {
             <MDBCol lg="6" md="12" className="mb-5">
               <MDBCol md="4" lg="6" className="float-left">
                 <img
-                  src="https://mdbootstrap.com/img/Photos/Avatars/img%20(32).jpg"
+                  src={img}
                   className="mx-auto mb-md-0 mb-4 rounded z-depth-1 img-fluid"
                   tag="img"
                   alt="Sample avatar"
