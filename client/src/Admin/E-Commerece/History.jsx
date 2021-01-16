530e94b8b1dc40d1e442e20ad5dda24aecde7ae3
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2021-01-16 21:24:37 +0500

    jkljkl

--- client/src/Admin/E-Commerece/History.jsx
+++ client/src/Admin/E-Commerece/History.jsx
@@ -6,13 +6,14 @@ import { MDBTable, MDBTableBody,MDBRow,MDBCol, MDBTableHead, MDBContainer } from
 import Axios from 'axios';
 
 export default function History() {
-    const [history,setHistory]=useState({});
+    const [history,setHistory]=useState([]);
     useEffect(() => {
         async function fetchData() {
           const response = await Axios.get("http://localhost:5000/history/get", {
             headers: { "x-auth-token": localStorage.getItem("auth-token") },
           });
           setHistory(response.data);
+          console.log(response.data);
         }
         fetchData();
       }, [history]);
@@ -20,35 +21,9 @@ export default function History() {
         <div>
             <Admin>
       <SideNav />
-      <MDBContainer>
-        <h2>List of Products : </h2>
-        {History.length === 0 ? (
-          <h2>Nothing to diplay</h2>
-        ) : (
-          <MDBRow>
-                        <MDBCol sm="6">
-                          <div>
-                            <b>Name :</b> {oneOrder.name}
-                          </div>
-                        </MDBCol>
-                        <MDBCol sm="6">
-                          <div>
-                            <b>Phone No :</b> {oneOrder.phoneNumber}
-                          </div>
-                        </MDBCol>
-                      </MDBRow>
-                      <MDBRow>
-                        <MDBCol sm="6">
-                          <div>
-                            <b>Delivery Address :</b> {oneOrder.address}
-                          </div>
-                        </MDBCol>
-                        <MDBCol sm="6">
-                          <div>
-                            <b>Order Date :</b> {oneOrder.orderDate}
-                          </div>
-                        </MDBCol>
-                      </MDBRow>
+      <br/>
+      <MDBContainer>  
+         
       </MDBContainer>
     </Admin>
         </div>
