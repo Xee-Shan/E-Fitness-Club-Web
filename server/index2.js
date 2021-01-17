926d3a84e19671c87bda3ce56d205b34bf702fdb
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2021-01-17 22:04:13 +0500

    lkl;k

--- server/index2.js
+++ server/index2.js
@@ -47,9 +47,7 @@ io.on("connect", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    const user = removeUser(socket.id);
-    console.log(socket);
-    
+    const user = removeUser(socket.id);    
     if (user) {
       io.to(user.room).emit("message", {
         user: "Admin",
