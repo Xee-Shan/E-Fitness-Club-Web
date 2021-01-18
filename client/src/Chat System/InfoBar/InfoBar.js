0946d504ac8eb6bec3e681d211aa2c854f678781
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2021-01-18 14:47:54 +0500

    kl;;kl

--- client/src/Chat System/InfoBar/InfoBar.js
+++ client/src/Chat System/InfoBar/InfoBar.js
@@ -3,16 +3,27 @@ import onlineIcon from '../../icons/onlineIcon.png';
 import closeIcon from '../../icons/closeIcon.png';
 import styles from './InfoBar.module.css';
 import { useHistory } from "react-router-dom";
-
+import Axios from "axios";
 const InfoBar = ({ room,socket }) => {
   const history = useHistory();
 
-  const btnClicked=(e)=>{
+  const btnClicked=async (e)=>{
     e.preventDefault();
-    socket.emit("disconnect");
-    socket.off();
-    history.push("/join");
-    window.location.reload();
+   const response=await Axios.get("http://localhost:5000/users/getUser", {
+      headers: { "x-auth-token": localStorage.getItem("auth-token") },
+    });
+      if (response.data.role === "user") {
+        socket.emit("disconnect");
+        socket.off();
+        history.push("/join");
+        window.location.reload();
+      }
+      if (response.data.role === "physiatrist") {
+        socket.emit("disconnect");
+        socket.off();
+        history.push("/doctor/home");
+        window.location.reload();
+      }    
  }
  
   
