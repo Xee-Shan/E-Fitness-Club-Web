b737ff0173d895ea2f8d9b3541f3787945825dca
Xee-Shan <61282797+Xee-Shan@users.noreply.github.com>
2021-01-19 23:21:51 +0500

    jkljkj

--- client/src/Doctor/SideNav/SidebarData.js
+++ client/src/Doctor/SideNav/SidebarData.js
@@ -2,7 +2,10 @@ import React from "react";
 //import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 //import * as IoIcons from "react-icons/io";
-
+import * as BsIcons from "react-icons/bs";
+import * as ImIcons from "react-icons/im";
+import * as MdIcons from "react-icons/md";
+import * as FaIcons from "react-icons/fa";
 
 export const SidebarData = [
   {
@@ -17,28 +20,28 @@ export const SidebarData = [
   {
     title: "Chat with patients",
     path: "",
-    icon: <AiIcons.AiFillHome />,
+    icon: <BsIcons.BsFillChatDotsFill />,
     cName: "nav-text",
   },
   {
     title: "Write a Blog",
     path: "/doctor/createBlog",
-    icon: <AiIcons.AiFillHome />,
+    icon: <ImIcons.ImBlog />,
     cName: "nav-text",
   },{
     title: "My Blogs",
     path: "/doctor/getBlog",
-    icon: <AiIcons.AiFillHome />,
+    icon: <ImIcons.ImBlogger />,
     cName: "nav-text",
   },{
     title: "Upload Meditation",
     path: "/doctor/upload",
-    icon: <AiIcons.AiFillHome />,
+    icon: <FaIcons.FaFileAudio />,
     cName: "nav-text",
   },{
     title: "My Meditation",
     path: "/doctor/meditation",
-    icon: <AiIcons.AiFillHome />,
+    icon: <MdIcons.MdAudiotrack />,
     cName: "nav-text",
   },{
     title: "My Profile",
